2ccfac07b18beb89aee9c3e8a6e831ae1e654fd9
unknown <felix4hubert@gmail.com>
2019-07-06 01:47:16 -0400

    Add unit testing for database rules

--- src/__tests__/database.test.ts
+++ src/__tests__/database.test.ts
@@ -41,15 +41,55 @@ afterAll(async () => {
  * ============
  */
 
-describe("Database rules", () => {
-  it("should deny guess to look at recipes", async () => {
+describe("Recipes rules", () => {
+  it("should deny guess to read or write recipes", async () => {
+    //arrange
     const db = authedApp(null);
+    //assert
     await firebase.assertFails(db.collection("recipes").get());
+    await firebase.assertFails(db.collection("recipes").doc("recipe").set({}));
   });
 
+  it("should allow a user to consult or create his own recipes", async () => {
+    //arrange    
+    const authDbAlice = authedApp({ uid: "alice" });
+    //act
+    //assert
+    await firebase.assertSucceeds(authDbAlice.collection("recipes").doc("recipe").set({ uid: "alice" }));
+    await firebase.assertSucceeds(authDbAlice.collection("recipes").doc("recipe").get());
+  });
+
+  it("should not allow a user to consult someone else's recipe", async () => {
+    //arrange    
+    const authDbAlice = authedApp({ uid: "alice" });
+    const authDbBob = authedApp({ uid: "bob" });
+    //act
+    await authDbAlice.collection("recipes").doc("recipe").set({ uid: "alice" });
+    //assert
+    await firebase.assertFails(authDbBob.collection("recipes").doc("recipe").get());
+    await firebase.assertSucceeds(authDbAlice.collection("recipes").doc("recipe").get());
+  });
+  
+  it("should not allow a user to create a recipe for another user", async () => {
+    //arrange    
+    const authDbAlice = authedApp({ uid: "alice" });
+    const authDbBob = authedApp({ uid: "bob" });
+    //act
+    //assert
+    await firebase.assertFails(authDbBob.collection("recipes").doc("recipe").set({ uid: "alice" }));
+    await firebase.assertFails(authDbAlice.collection("recipes").doc("recipe").set({ uid: "bob"}));
+  });
+});
+
+describe("Cart rules", () => {
   it("should only allow a user to consult his own cart", async () => {
-    const db = authedApp({ uid: "alice" });
-    await firebase.assertFails(db.collection("carts").doc("bob").get());
-    await firebase.assertSucceeds(db.collection("carts").doc("alice").get());
+    //arrange
+    const authDbAlice = authedApp({ uid: "alice" });
+    //act
+    authDbAlice.collection("carts").doc("bob").set({});
+    authDbAlice.collection("carts").doc("alice").set({});
+    //assert
+    await firebase.assertFails(authDbAlice.collection("carts").doc("bob").get());
+    await firebase.assertSucceeds(authDbAlice.collection("carts").doc("alice").get());
   });
 });
