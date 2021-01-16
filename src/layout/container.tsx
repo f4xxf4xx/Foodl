e3ad4778471b3a5f03952b3faa88205ef1625c19
Laurent O'Donoughue <l.odonoughue@gmail.com>
2021-01-16 17:13:30 -0500

    fix scrollIntoView that scrolls below the header

--- src/layout/container.tsx
+++ src/layout/container.tsx
@@ -9,6 +9,7 @@ export const Container = styled.div<Props>`
   width: 100%;
   max-width: ${({ theme }) => theme.breakpoints.large}px;
   margin: 0 auto;
+  scroll-margin-top: ${({ theme }) => theme.sizes.headerHeight};
 
   @media (max-width: ${({ theme }) => theme.breakpoints.large-1}px) {
     max-width: ${({ theme }) => theme.breakpoints.medium}px;
