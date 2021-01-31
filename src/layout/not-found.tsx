4e5780f1128133e7271ca84abe72244b909fe258
Laurent O'Donoughue <l.odonoughue@gmail.com>
2021-01-31 11:00:27 -0500

    animate not-found page

--- src/layout/not-found.tsx
+++ src/layout/not-found.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useContext, useRef } from "react";
+import styled, { ThemeContext } from "styled-components";
+import { motion } from "framer-motion";
+import { useHasBeenViewed } from "hooks/use-has-been-viewed";
+import { useStaggerEffect } from "hooks/use-stagger-effect";
 import { SpinningBox } from "components/spinning-box";
 import { Container } from "layout/container";
 import { Theme } from "theme";
@@ -13,23 +16,33 @@ const StyledContainer = styled(Container as any)`
   justify-content: center;
 `;
 
-const StyledCode = styled.span`
+const StyledCode = styled(motion.span)`
   font-size: 10rem;
 `;
 
-const StyledDescription = styled.p<{ theme: Theme}>`
+const StyledDescription = styled(motion.p)<{theme: Theme}>`
   font-size: ${({theme}) => theme.fontSizes.large};
   margin: ${({theme}) => theme.space.large}px 0;
   text-transform: uppercase;
   text-align: center;
 `;
 
-export const NotFound: React.FC = () => (
-  <StyledContainer>
-    <SpinningBox>
-      <StyledCode>404</StyledCode>
-      <StyledDescription>This plate looks empty</StyledDescription>
-      <img src={EmptyPlate} alt="Empty plate" />
-    </SpinningBox>
-  </StyledContainer>
-);
+export const NotFound: React.FC = () => {
+  const getAnchor = rect => ({ x: rect.x, y: rect.y });
+  const theme = useContext<Theme>(ThemeContext);
+  const containerRef = useRef();
+  const hasBeenViewed = useHasBeenViewed(containerRef);
+  const codeStagger = useStaggerEffect<HTMLSpanElement>(hasBeenViewed, theme, containerRef, getAnchor);
+  const descriptionStagger = useStaggerEffect<HTMLParagraphElement>(hasBeenViewed, theme, containerRef, getAnchor);
+  const imgStagger = useStaggerEffect<HTMLImageElement>(hasBeenViewed, theme, containerRef, getAnchor);
+
+  return (
+    <StyledContainer ref={containerRef}>
+      <SpinningBox>
+        <StyledCode {...codeStagger}>404</StyledCode>
+        <StyledDescription {...descriptionStagger}>This plate looks empty</StyledDescription>
+        <motion.img {...imgStagger} src={EmptyPlate} alt="Empty plate" />
+      </SpinningBox>
+    </StyledContainer>
+  );
+}
